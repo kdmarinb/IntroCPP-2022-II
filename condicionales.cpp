8b0663280cb5f2f67b251d469fc3c9ad0bf14738
oquendo -- 2516 - FÍSICA -- "Marin Baez Karen Daniela <kmarin@sala25.salafis.net>
2022-08-17 03:48:19 -0500

    Condicionales

---
+++ condicionales.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main(void) {
+  
+  int m{1};
+     if (m < 3) {
+       std::cout << "Opcion 1\n";
+     }
+     else if (m >= 4 & m <= 7) {
+       std::cout << "Opción 2\n";
+     }
+     
+     else
+       std::cout << "Opción 3\n";
+
+ return 0;
+
+}
